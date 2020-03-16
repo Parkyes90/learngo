5eb50f73d17c273f6cb88c28926c07ff76175383
parkyes90 <parkyes90@gmail.com>
2020-03-16 17:34:31 +0900

    Add goroutine url checker

--- main.go
+++ main.go
@@ -1,19 +1,51 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"github.com/parkyes90/learngo/custom_dict"
+	"net/http"
 )
 
-func main() {
-	dictionary := custom_dict.Dictionary{"first": "First word"}
-	def, err := dictionary.Search("first")
+var errRequestFailed = errors.New("network error")
 
-	err2 := dictionary.Add("test", "test2")
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(def)
+func main() {
+	c := make(chan error)
+	var results = make(map[string]string)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
 	}
-	fmt.Println(dictionary, err2)
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+	for _, url := range urls {
+		result := "OK"
+		err := <-c
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+func hitURL(url string, c chan<- error) {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		c <- err
+	} else if resp.StatusCode >= 400 {
+		fmt.Println(resp.StatusCode)
+		c <- errRequestFailed
+	} else {
+		c <- nil
+	}
+
 }
